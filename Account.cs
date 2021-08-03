594b9fa4445f58b1468598617eb0840c1e9738fe
VanishreeVH <88375535+VanishreeVH@users.noreply.github.com>
2021-08-03 15:40:43 +0530

    Second commit

--- Account.cs
+++ Account.cs
@@ -4,7 +4,61 @@ using System.Text;
 
 namespace MyFirstConsole
 {
-    class Class1
+    class Account
     {
+        string username;
+        Int64 contact;
+        string email;
+        string address;
+        Item[] Kart;
+        Item[] orders;
+        public void setusername(String username)
+        {
+            this.username = username;
+        }
+        public String getusername()
+        {
+            return username;
+        }
+
+        public void setcontact(Int64 contact)
+        {
+            this.contact = contact;
+        }
+        public Int64 getcontact()
+        {
+            return contact;
+        }
+        public void setemail(String email)
+        {
+            this.email = email;
+        }
+        public String getemail()
+        {
+            return email;
+        }
+        public void setaddress(String address)
+        {
+            this.address = address;
+        }
+        public String getaddress()
+        {
+            return address;
+        }
+        public void setKart(Item[] kart)
+        {
+            this.Kart = kart;
+        }
+        public Item[] getKart()
+        {
+            return Kart;
+        }
+        public void setorders(Item[] orders)
+        {
+            this.orders = orders;
+        }
+        public Item[] getorders()
+        {
+            return orders;
+        }
     }
-}
